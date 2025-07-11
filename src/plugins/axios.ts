016175f3e624d98b7f60397e8b179febe2d1f43b
John Garcia <jgarc613@intcomex.com>
2025-07-11 17:38:42 -0500

    Se configura .env

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_BACKEND_API_URL || "http://localhost:3000"
+
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL,
     timeout: 3000,
   });
 
